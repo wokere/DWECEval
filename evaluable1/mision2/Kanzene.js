6a81ab10c117e57cdd4078e1e9c8384af28086d5
Loli Gonzalez <lolinams@gmail.com>
2020-10-28 18:06:57 +0100

    comentarios

--- evaluable1/mision2/Kanzene.js
+++ evaluable1/mision2/Kanzene.js
@@ -1,13 +1,15 @@
-
+//Establezco los datos del hospital que en principio no cambiaran,
 const NOMBREHOSPITAL = "Kanzene Hospital";
 const LOCALIDADHOSPITAL = "Kanzene";
 const RESPONSABLEHOSPITAL = "Carmencita Diego";
-
+//cargo la gestion del hospital
 window.onload = inicio;
 
 function inicio(){
+    //inicio hospital
     let hospitalK = new Hospital(NOMBREHOSPITAL, LOCALIDADHOSPITAL, RESPONSABLEHOSPITAL);
+    //y la gestion del hospital que he iniciado
     let GestionKanzene = new GestionHospital(hospitalK);
-    //muestro en el documento los datos del hospital y asigno los eventos
+    //lanza la gestión en el navegador con los datos del hospital y establece los eventos princpales
     GestionKanzene.lanzarGestion();
 }
