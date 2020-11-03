a0a9902fd2c2bfc241b4894bd65fd9bacb11c176
Loli Gonzalez <lolinams@gmail.com>
2020-11-03 16:58:08 +0100

    validacion

---
+++ evaluable1/mision4/elementosFormulario.js
@@ -0,0 +1,67 @@
+"use strict"
+
+//crea el input tipo texto con su expresionRegular, className, placeHolder, si es obligatorio y un titulo y lo devuelve
+function crearInputText(regExp, clase,placeHolder) {
+
+    let input = document.createElement("INPUT");
+    input.type = "text";
+    input.className = clase;
+    input.placeholder=placeHolder;
+    input.setAttribute("regexp", regExp);
+
+    return input;
+}
+//crea un submit con un evento y una clase y lo deuelve
+function crearSubmit(clase) {
+    let boton = document.createElement("input");
+    boton.type="submit";
+    boton.value = "Validar";
+    boton.className = clase;
+    return boton;
+}
+
+
+function crearFormulario(elementos) {
+
+    let f =document.createElement("form");
+    f.className="virus";
+    for(let i=0;i<elementos.length;i++){
+        f.appendChild(elementos[i]);
+    }
+    return f;
+
+}
+
+//manejador de los botones. Coge los datos por la clase del elemento en el que se ha hecho clic 
+//y tras comprobar si cumple el input con la expresión regular pone el borde verde  o rojo. Si todo
+//esta bien manda una alerta 
+function validarEnvio() {
+   
+    let datos = document.getElementsByClassName(this.className);
+    let valido =true;
+    for(let i=0;i<datos.length;i++){
+
+        if(datos[i].type == "text"){
+            
+            let regExpInput = new RegExp(datos[i].getAttribute("regexp"));
+            let esValido = regExpInput.test(datos[i].value);
+            if (esValido) {
+           
+                datos[i].style.borderColor="green";
+                
+            } else {
+
+                datos[i].style.borderColor="red";
+                valido =false;
+
+            }
+        }
+
+    }
+    if(valido){
+        
+        alert("Todo correcto!");
+    }
+    return valido;
+   
+}
--- evaluable1/mision4/script.js
+++ evaluable1/mision4/script.js
@@ -1,35 +1,52 @@
-window.onload =  isLogged;
+"use strict"
 
-    
+window.onload = isLogged;
 
-    
-function isLogged(){
+function isLogged() {
 
     let valido = localStorage.getItem("valid");
     let formulario = document.getElementById("formulario");
-    if(valido == null){
-       
-        formulario.onsubmit =validateUserPW;
+    if (valido == null) {
+
+        formulario.onsubmit = validateUserPW;
+    } else if (valido =="true"){
+        muestraDatosUsuarioLogueado();
+
     }else{
-        
-       formulario.parentNode.removeChild(formulario); 
-       document.getElementById("miOtroForm").innerHTML = "logueado";
-        
-}
+        formulario.onsubmit = validateUserPW;
+        alert ("tramposillo");
     }
+}
 
+function validateUserPW() {
 
+    let userPw = document.getElementById("pw").value;
 
-function validateUserPW(){
+    if (userPw == "supercalifragilistico") {
 
-    let userPw= document.getElementById("pw").value;
-
-    if(userPw =="supercalifragilistico"){
-        localStorage.setItem("valid","true");
+        localStorage.setItem("valid", "true");
+        muestraDatosUsuarioLogueado();
         return true;
-    }else{
+    } else {
         alert("contraseña no valida");
         return false;
-       
+
     }
 }
+
+function muestraDatosUsuarioLogueado() {
+
+    formulario.parentNode.removeChild(formulario)
+    let inputNombre = crearInputText("[a-z]","virus","Nombre del Investigador");
+    let inputNumero =crearInputText("[a-z]","virus", "Número de investigador");
+    let inputID= crearInputText("[a-z]","virus","ID de la firma del virus");
+    let inputFirma = crearInputText("[a-z]","virus","Firma");
+    let button = crearSubmit("virus");
+    let f = crearFormulario([inputNombre,inputNumero,inputID,inputFirma,button]);
+
+    document.getElementById("miOtroForm").appendChild(f);
+
+    
+    f.onsubmit= validarEnvio;
+}
+
