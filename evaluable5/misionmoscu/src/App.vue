43b617044420612ca0b4e0ddc7f4a8994edc68af
Loli <wokere@gmail.com>
2021-01-21 16:37:11 +0100

    init mision moscu

---
+++ evaluable5/misionmoscu/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+{{info}}
+  </div>
+</template>
+
+<script>
+//importamos axios para usarlo 
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data:function(){
+    return{
+      info: null
+    }
+  },
+    mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<style>
+
+</style>
