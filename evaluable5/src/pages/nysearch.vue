36e2916a23af49734bc75a8840648496c97e240c
Loli <wokere@gmail.com>
2021-01-22 00:55:14 +0100

    added axios call to nyt

--- evaluable5/src/pages/nysearch.vue
+++ evaluable5/src/pages/nysearch.vue
@@ -1,9 +1,9 @@
 <template>
   <q-page class="flex flex-center">
-    <form @submit.prevent.stop="onSubmit" class="busqueda">
+    <q-form @submit.prevent.stop="onSubmit" class="busqueda">
     <q-input
       class="input-form"
-      v-model="text"
+      v-model.trim="text"
       filled
       label="texto de busqueda"
       hint="introduce texto"
@@ -11,22 +11,24 @@
     />
     <q-input
       class="input-form"
-      v-model="date"
+      v-model.trim="mindate"
       filled
       label="Fecha minima"
       mask="########"
       hint="Formato YYYYMMAA"
-     :rules="[valor => !!valor || 'Campo Obligatorio']"
+      reactive-rules
+     :rules="[dataFormat,checkDateInterval]"
 
     />
     <q-input
       class="input-form"
-      v-model="date"
+      v-model.trim="maxdate"
       filled
       label="fecha maxima"
       mask="########"
       hint="Formato YYYYMMAA"
-    :rules="[valor => !!valor || 'Campo Obligatorio']"
+      reactive-rules
+     :rules="[dataFormat, checkDateInterval]"
 
     />
     <!-- ponemos emit-value para q v-model coja value,mirar porque no autorellena-->
@@ -34,14 +36,15 @@
       v-model="order"
       filled
       label="Ordenar"
-      emit-value
       :options="opciones"
       hint="elige"
      :rules="[valor => !!valor || 'Campo Obligatorio']"
 
     />
+    {{order}}
     <q-btn color="primary" type="submit" class="boton" size="lg" round icon="search" />
-    </form>{{ date }}
+    </q-form>
+    {{news}}
     <q-list>
         <q-item v-for="n in 6" :key="n">
              <q-item-section>
@@ -67,12 +70,46 @@ export default {
       order: null,
       //objetos label-value-icon
       opciones: [
-        { label: "Más recientes", value: "old" },
-        { label: "Más antiguas", value: "new" },
+        { label: "Más recientes", value: "newest" },
+        { label: "Más antiguas", value: "oldest" },
       ],
-      date: null,
+      mindate: null,
+      maxdate: null,
+      news:[]
     };
   },
+  methods:{
+      dataFormat(value){
+          const rgx = new RegExp("\\d{8}");
+          return rgx.test(value) ? null : "FORMATO AAAMMDD";
+
+      },
+      onSubmit(){
+          //como la validacion se ha hecho en los propios componentes del formulario
+          //con las rules
+          //podemos enviarlo sin más
+           let query = "q="+this.text;
+           let begindate = "begin_date="+this.mindate;
+           let enddate = "end_date="+this.maxdate;
+           let order = "sort="+this.order.value;
+           const API = "api-key=mkdTSzg999FowCJ2wnMVRKGIsOGDZHqT";
+           let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+begindate+"&"+enddate+"&"+order+"&"+query+"&"+API;
+
+          this.$axios.get(url)
+          .then(response=>{
+              console.log(response);
+              this.news = response.data.response.docs;
+          })
+          .catch(error =>{
+              console.log(error);
+          })
+         // https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=20120101&end_date=20120101&q=some&sort=newest&api-key=mkdTSzg999FowCJ2wnMVRKGIsOGDZHqT
+      },
+      checkDateInterval(){
+          console.log(this.maxdate - this.mindate > 0);
+          return this.maxdate - this.mindate > 0 ? null : "La fecha ha de ser anterior a la maxima";
+      }
+  }
 };
 </script>
 <style scoped>
